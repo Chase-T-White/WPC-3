d5d7fdf718d452ef7b74c182070b773559291733
cwhite70 <chase.t.white@hotmail.com>
2023-06-26 19:42:26 -0600

    Service pages update

--- app/services/builds/page.jsx
+++ app/services/builds/page.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PageHeader from "@/app/Components/PageHeader";
+import { GiPolarStar } from "react-icons/gi";
 
 const page = () => {
   const heading = "Building & Renovations";
@@ -9,6 +10,30 @@ const page = () => {
   return (
     <main>
       <PageHeader heading={heading} text={text} image={image} />
+      <section>
+        <article className="w-screen max-w-[1400px] mx-auto">
+          <div>
+            <h2 className="text-center mb-4 text-primary-color mb-12">
+              Well, What Do You Need Done?
+            </h2>
+            <p className="max-w-[100ch] mx-auto mb-40 px-4">
+              That's our simple pitch to anyone that asks what it is that we do.
+              We're serious, if you asked us to manage the construction of a
+              space station on the moon, we'd figure out exactly how to get it
+              done. However, our services are more often requested for projects
+              that are a little closer to home. From residential remodels to
+              commercial gravel work, and everything in between, there's rarely
+              a task we can't handle!
+            </p>
+          </div>
+          <div className="flex justify-center items-center mb-40">
+            <GiPolarStar className="text-primary-color text-4xl" />
+          </div>
+          <div className="mb-20">
+            <h3 className="text-primary-color mb-12">We Have Experience In:</h3>
+          </div>
+        </article>
+      </section>
     </main>
   );
 };
--- app/services/oelo/page.jsx
+++ app/services/oelo/page.jsx
@@ -6,28 +6,49 @@ import Image from "next/image";
 const page = () => {
   const heading = "OELO Permanent LED Lighting";
   const text =
-    "White Peak Contracting is proud to announce that we are now Montana's best for premium residential and commercial permanent LED lighting through our new partnership with OELO! OELO's products are proudly manufactured in the Rocky Mountains and are packed full of exclusive features that you can't get through the competition! That's why White Peak Contracting chose them as our product of choice. We are as local as you can get, and we hang high upon our reputation within our community; OELO's products stands along with our mission to provide you with the best service, the best products, to ensure your best experience!";
+    "OELO's permanent LED home lighting technology puts endless color combinations and breathtaking curb appeal in the palm of your hand. Feel free to check out the OELO website to learn more about the tech, then contact us when you're ready to get started!";
   const image = "/images/Home/holidayLights2.jpg";
   return (
     <main>
       <PageHeader heading={heading} text={text} image={image} />
-      <section>
-        <article className="w-screen max-w-[1200px] mx-auto">
-          <h3 className="text-primary-color text-center mb-4">
-            OELO Lighting Solutions
-          </h3>
+      <section className="mt-48 w-screen grid grid-cols-[1fr,_2fr] gap-[150px] px-8">
+        <article>
+          <h3 className="text-primary-color mb-4">OELO Lighting Solutions</h3>
           <div>
             <p className="mb-16">
-              OELO's permanent LED home lighting technology puts endless color
-              combinations and breathtaking curb appeal in the palm of your
-              hand. Feel free to check out the OELO website to learn more about
-              the tech, then contact us when you're ready to get started!
+              White Peak Contracting is proud to announce that we are now
+              Montana's best for premium residential and commercial permanent
+              LED lighting through our new partnership with OELO! OELO's
+              products are proudly manufactured in the Rocky Mountains and are
+              packed full of exclusive features that you can't get through the
+              competition! That's why White Peak Contracting chose them as our
+              product of choice. We are as local as you can get, and we hang
+              high upon our reputation within our community; OELO's products
+              stands along with our mission to provide you with the best
+              service, the best products, to ensure your best experience!
             </p>
-            <button>
-              <Link href="https://www.oelo.com/" target="_blank">
-                OELO Website
-              </Link>
-            </button>
+            <div className="flex gap-16">
+              <button className="btn btn__primary">
+                <Link href="https://www.oelo.com/" target="_blank">
+                  Contact Now
+                </Link>
+              </button>
+              <button className="btn">
+                <Link href="https://www.oelo.com/" target="_blank">
+                  OELO Website
+                </Link>
+              </button>
+            </div>
+          </div>
+        </article>
+        <article>
+          <div className="relative h-full">
+            <Image
+              src={"/images/Services/OELO/oelo1.jpg"}
+              alt="OELO Lights on a building"
+              fill
+              className="object-center object-cover"
+            />
           </div>
         </article>
       </section>
